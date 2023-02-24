1f34acfc48c32bde27a3dea7a936818949541f0e
MariamiKvirkvia <121309330+MariamiKvirkvia@users.noreply.github.com>
2023-02-24 06:43:37 -0500

    Updated some spellings.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-console.log("Hello, I am the first JS lesson");
+console.log("Hello, I am the first JS lesson.");
 
 // Number
 let age = 18;
@@ -15,7 +15,7 @@ console.log("Type of myBoolean is", typeof myBoolean, "and Type of yourBoolean i
 
 // Undefined
 let someUndefined;
-console.log("Type of someUnefined is", typeof someUndefined);
+console.log("Type of someUndefined is", typeof someUndefined);
 
 // Null
 let nullData = null;
@@ -37,20 +37,20 @@ console.log(sentence);
 
 
 // Let's check how smart is my browser :)
-console.log(`Let's check how smart is my browser`);
+console.log(`Let's check how smart is my browser.`);
 const num1 = 456382;
 const num2 = 790352;
 const num3 = 109378;
 const num4 = 999997;
 
 let sum = num1 + num4;
-console.log(`Sum of ${num1} and ${num2} is ${sum}`);
+console.log(`Sum of ${num1} and ${num2} is ${sum}.`);
 let substract = sum - num2;
-console.log(`When ${num2} is substracted from ${sum} we are getting ${substract}`);
+console.log(`When ${num2} is substracted from ${sum} we are getting ${substract}.`);
 let division = sum / substract;
-console.log("To devide", sum, "by", substract, "the browser will get", division);
+console.log("To devide", sum, "by", substract, "the browser will get", division,".");
 let multiply = num3 * num2;
-console.log("Can my browser guess what is", num3, "multiplied by", num2, "is? - The answer is:", multiply);
+console.log("Can my browser guess what is", num3, "multiplied by", num2, "? - The answer is:", multiply);
 let modulo = sum % multiply;
 console.log(`Final queston: what is the modulo of ${sum} devided by ${multiply}? - The answer is ${modulo}`);
 console.log(`Okay, browser seems to have some brain :)`);
