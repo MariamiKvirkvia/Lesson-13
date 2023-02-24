43b247f3746f2e77bcef7d8fe842f746b198d304
MariamiKvirkvia <121309330+MariamiKvirkvia@users.noreply.github.com>
2023-02-24 02:48:55 -0500

    Added primitive data types and pulled information about their types in console.

---
+++ app.js
@@ -0,0 +1,22 @@
+console.log("Hello, I am the first JS lesson");
+
+// Number
+let age = 18;
+console.log("Type of age is", typeof age);
+
+// String
+let text = "welcome";
+console.log("Type of text is", typeof text);
+
+// Boolean
+const myBoolean = true;
+const yourBoolean = false;
+console.log("Type of myBoolean is", typeof myBoolean, "and Type of yourBoolean is also", typeof yourBoolean);
+
+// Undefined
+let someUndefined;
+console.log("Type of someUnefined is", typeof someUndefined);
+
+// Null
+let nullData = null;
+console.log("Type of nullData is", typeof nullData);
